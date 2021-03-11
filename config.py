0eed0225248e442242fb62e2ae21c1e4e2d65a3d
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-12 01:56:46 +0300

    add weather api

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY=os.environ.get('SECRET_KEY')
-    WEATHER_API_KEY=os.environ.get('WEATHER_API_KEY')
+    SECRET_KEY=os.environ.get('SECRET_KEY') or 'fhvnbmgmgkh9ttolslso'
+    WEATHER_API_KEY=os.environ.get('WEATHER_API_KEY') or 'a778d9642410a11ed2cbd17c20c246bc'
+    SQLALCHEMY_DATABASE_URI= 'sqlite:///weather.db'
 
 
 class ProdConfig(Config):
