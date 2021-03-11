fc0926e46a13c1f7ceae67b9adecd7a5b39b7e3c
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-03-12 01:58:36 +0300

    initialize db

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import config_options
+from config import config_options,Config
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 bootstrap=Bootstrap()
 
 def create_app(config_name):
 
     app=Flask(__name__)
 
-    app.config.from_object(config_options[config_name])
 
+    app.config.from_object(Config)
+    # app.config.from_object(config_options[config_name])
+    # app.config[Config]
+   
+
+    db.init_app(app)
     bootstrap.init_app(app)
+
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
